d7f080cf0bc23eaa0eb71a3bbeaaf84af29fb6f4
James Royal <jroyal@cloudflare.com>
2021-04-28 15:48:48 -0500

    Allow for piping

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 #!/usr/bin/env node
 const moment = require("moment");
 
-let form = "dddd, MMMM D YYYY, h:mm:ss a z";
+const form = "dddd, MMMM D YYYY, h:mm:ss a z";
 
-if (process.argv.length != 3) {
-  console.log("usage: epoch <unix timestamp>");
-  process.exit(1);
+function print(ts) {
+  let t = moment.unix(ts);
+  console.log(t.utc().format(form));
+  console.log(t.local().format(form));
+  console.log(t.fromNow());
 }
 
-const ts = process.argv[2];
-let t = moment.unix(ts);
-console.log(t.utc().format(form));
-console.log(t.local().format(form));
-console.log(t.fromNow());
+if (process.stdin.isTTY) {
+  if (process.argv.length != 3) {
+    console.log("usage: epoch <unix timestamp>");
+    process.exit(1);
+  }
+
+  const ts = process.argv[2];
+  print(ts);
+} else {
+  data = "";
+  process.stdin.setEncoding("utf-8");
+
+  process.stdin.on("readable", function () {
+    var chunk;
+    while ((chunk = process.stdin.read())) {
+      data += chunk;
+    }
+  });
+
+  process.stdin.on("end", function () {
+    print(data);
+  });
+}
